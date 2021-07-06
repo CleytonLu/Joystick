b0cd56ec329a44b44c187540d67ac33c0e9e8dd0
CleytonLu <cleytonlucasiurd@gmail.com>
2021-07-06 10:41:42 -0300

    controle com click e click continuo

--- src/Joystick/Joystick.java
+++ src/Joystick/Joystick.java
@@ -5,7 +5,7 @@ public class Joystick {
     public static void main(String[] args) {
         
        Botões p1 = new ComandosDoControle();
-       
+       p1.L1();
        
        Botões p2 = new ComandosDoControle();
        
